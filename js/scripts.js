a1dd7aaaaf3be8f063ddad7dc5b09912b325adf5
tamperic <tamaraperic.work@gmail.com>
2024-12-02 13:00:57 +0100

    created a new variable
    and assigned an array to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Butterfree', height: 11, types: ['flying', 'poison']},
+    {name: 'Rattata', height: 3, types: ['fighting', 'psychic']},
+    {name: 'Wigglytuff', height: 10, types: ['ice', 'ghost']},
+    {name: 'Meowth', height: 4, types: ['steel', 'electric']}
+];
