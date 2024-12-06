2be9c200ce0e9d3e203395e1e34b5afaaa5921eb
tamperic <tamaraperic.work@gmail.com>
2024-12-06 22:10:28 +0100

    added a loop to Pokemon's array list
    and added a conditional to highlight special Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
+// array list of Pokemons
 let pokemonList = [
     {name: 'Butterfree', height: 11, types: ['flying', 'poison']},
     {name: 'Rattata', height: 3, types: ['fighting', 'psychic']},
     {name: 'Wigglytuff', height: 10, types: ['ice', 'ghost']},
     {name: 'Meowth', height: 4, types: ['steel', 'electric']}
 ];
+let text = '';
+
+// loop that iterates over each item in Pokemon's list
+for (let i = 0; i < pokemonList.length; i++)  {
+    if (pokemonList[i].height > 10) {
+        text = ' - Wow, that’s big!';
+    } else {
+        text = '';
+    }
+    document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) ${text} </p>`);
+}
