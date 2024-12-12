48a19b8faafff3ec8b91cc5e2035aab90b0afcc8
tamperic <tamaraperic.work@gmail.com>
2024-12-12 19:41:04 +0100

    created IIFE and updated forEach() function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,41 @@
-// array list of Pokemons
-let pokemonList = [
-    {name: 'Butterfree', height: 11, types: ['flying', 'poison']},
-    {name: 'Rattata', height: 3, types: ['fighting', 'psychic']},
-    {name: 'Wigglytuff', height: 10, types: ['ice', 'ghost']},
-    {name: 'Meowth', height: 4, types: ['steel', 'electric']}
-];
+// IIFE 
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Butterfree', height: 11, types: ['flying', 'poison']},
+        {name: 'Rattata', height: 3, types: ['fighting', 'psychic']},
+        {name: 'Wigglytuff', height: 10, types: ['ice', 'ghost']},
+        {name: 'Meowth', height: 4, types: ['steel', 'electric']}
+    ];
 
+    //adds the Pokémon to the pokemonList array
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+
+    // returns a pokemonList array
+    function getAll() {
+      return pokemonList;
+    }
+    
+    return {
+      add: add,
+      getAll: getAll
+    };
+})();
+  
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Ninetales', height: 11, types: ['psychic', 'fire'] });
+console.log(pokemonRepository.getAll()); 
+  
 // forEach() that iterates over each item in Pokemon's list
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(`<p> ${pokemon.name} (height: ${pokemon.height}) </p>`);
     if (pokemon.height > 10) {
-         document.write(" - Wow, that’s big!");
-      } else {
+        document.write(" - Wow, that’s big!");
+    } else {
         document.write(" ");
-      } 
-});  
+    } 
+}); 
 
 /*
 let text = '';
