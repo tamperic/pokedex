bb39b1e1be0da9e1a52d202b511686724090ef37
tamperic <tamaraperic.work@gmail.com>
2025-01-13 11:37:33 +0100

    replaced modal in “index.html” with Bootstrap modal

--- js/scripts.js
+++ js/scripts.js
@@ -2,60 +2,7 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-
-    let modalContainer = document.querySelector('#modal-container');
-
-    function showModal(name, height, img) {
-        modalContainer.innerHTML = ''; // Clear all modal content
-        
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
-        
-        // Add the new modal content
-        let closeButtonElement = document.createElement('button');
-        closeButtonElement.classList.add('modal-close');
-        closeButtonElement.innerText = 'X';
-        closeButtonElement.addEventListener('click', hideModal); // Close the modal by clicking the "X" button
-        
-        let nameElement = document.createElement('h1');
-        nameElement.innerText = name;
-        
-        let heightElement = document.createElement('p');
-        heightElement.innerText = height;
-        
-        let imageElement = document.createElement('img');
-        imageElement.setAttribute('src', img);
-        imageElement.setAttribute('alt', 'The Pokémon icon');
-        
-        modal.appendChild(closeButtonElement);
-        modal.appendChild(nameElement);
-        modal.appendChild(heightElement);
-        modal.appendChild(imageElement);
-        modalContainer.appendChild(modal);
-        
-        modalContainer.classList.add('is-visible');
-    }
     
-    function hideModal() {
-        modalContainer.classList.remove('is-visible');
-    }
-    
-    // Close the modal by pressing the ESC key
-    window.addEventListener('keydown', (e) => {
-        if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-          hideModal();
-        }
-    })
-    
-    // Close the modal by clicking outside of the modal
-    modalContainer.addEventListener('click', (e) => {
-        let target = e.target;
-        if (target === modalContainer) {
-          hideModal();
-        }
-    })
-  
-
     // Add the Pokémon to the pokemonList array and checks if the typeof parameter is an object
     function add(item) {
         if (typeof item === 'object' ) {
@@ -76,12 +23,16 @@ let pokemonRepository = (function () {
 
     // DOM manipulation; add a list of buttons to the array
     function addListItem(item)  {
-        let list = document.querySelector('.pokemon-list'); // Assign the variable to the <ul> element
+        let list = document.querySelector('.list-group'); // Assign the variable to the <ul> element
 
         let listItem = document.createElement('li'); // Create a list element
+        listItem.classList.add('list-group-item'); // Add a class attribute to the li element
+
         let button = document.createElement('button'); // Create a button element
         button.innerText = item.name; // Add text of the Pokémon's names to the button element
-        button.classList.add('button-class'); // Add class attribute to the button element
+        button.classList.add('btn', 'btn-primary'); // Add a class attribute to the button element
+        button.setAttribute('data-toggle', 'modal'); // Add an attribute for event listener (created from Bootstrap)
+        button.setAttribute('data-target', '#detailsModal'); // Add an attribute on the button element to refer to the modal
 
         listItem.appendChild(button); // Append the button to the list item as its child
         list.appendChild(listItem); // Append the list item to the <ul> as its child
@@ -102,7 +53,6 @@ let pokemonRepository = (function () {
                     detailsUrl: item.url
                 };
                 add(pokemon);
-                console.log(pokemon);
             });
         }).catch(function (e) {
             console.error(e);
@@ -122,6 +72,22 @@ let pokemonRepository = (function () {
         }).catch(function (e) {
             console.error(e);
         });
+    }
+
+    function showModal(name, height, img) {
+        let nameElement = document.querySelector('.modal-title');
+        nameElement.innerText = name;
+        
+        let heightElement =  document.querySelector('.heightElement');
+        heightElement.innerText = height;
+        
+        let imageElement = document.querySelector('.imageElement');
+       imageElement.setAttribute('src', img);
+        
+        let modalBody = document.querySelector('.modal-body');
+        
+        modalBody.appendChild(imageElement);
+        modalBody.appendChild(heightElement);  
     }
 
     // Load the Pokémon details from the API
@@ -147,11 +113,3 @@ pokemonRepository.loadList().then(function () {
         pokemonRepository.addListItem(item);
     }); 
 });
-
-/*
-//  Add filter() function to find specific Pokémon by name
-function filterPokemon (name) {
-    let result = pokemonRepository.getAll().filter((item) => item.name === item);
-    console.log(result);
-}; 
-filterPokemon('Meowth'); */
